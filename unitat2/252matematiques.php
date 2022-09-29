47415c2635374c0d697d3f134df472932a30501d
nando <90838814+nandosendraa@users.noreply.github.com>
2022-09-30 01:09:46 +0200

    252

---
+++ unitat2/252matematiques.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>252</title>
+</head>
+<body>
+<?php
+func_get_args()
+?>
+</body>
+
+</html>
