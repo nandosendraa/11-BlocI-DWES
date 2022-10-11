a51f118575f76a61d6880df6e82fe168ba7ea89b
Nando <nandet2003@gmail.com>
2022-10-11 12:52:51 +0200

    Exceptions

---
+++ unitat3/Errors i excepcions/320-error-handling.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+include 'Employee.php';
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>320</title>
+</head>
+<body>
+<?php
+try {
+    $empleado = new Employee("Nando","Sendra",-100);
+}
+catch (Exception $exception){
+    echo "S'ha produït el següent error:". $exception-> getMessage ();
+}
+?>
+</body>
+
+</html>
